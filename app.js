dc6f5a95da55efa5bdff52788c817bc1271848dd
ben-23-96 <benspeakman123@gmail.com>
2022-09-18 21:56:37 +0100

    add session store

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const session = require('express-session')
+let MySQLStore = require('express-mysql-session')(session);
 const flash = require('express-flash')
 const cookieParser = require("cookie-parser")
 const path = require('path')
@@ -9,13 +10,20 @@ const connection = require('./db')
 
 const app = express()
 const port = process.env.PORT || 3000
+const sessionStore = new MySQLStore({
+    expiration: 86400000,
+    clearExpired: true,
+    checkExpirationInterval: 90000000,
+}, connection);
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, "views"));
 app.use(session({
     secret: 'secret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: sessionStore,
+    expires: 86400000,
 }));
 app.use(flash())
 app.use(cookieParser());
@@ -24,7 +32,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
+    console.log(req.session)
     if (req.session.loggedin === true) {
         username = req.session.name
         res.render('index', { loggedin: true, user: username })
@@ -42,28 +51,26 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 
-app.get('/dailyword', (req, res) => {
-    connection.query('SELECT * FROM dailyword', function (err, result, fields) {
-        if (err) {
-            console.log(err);
-        } else {
-            dailyWord = result[0]['word']
-            res.send({ word: dailyWord })
-        }
-    })
+app.get('/dailyword', async (req, res) => {
+    try {
+        const [result, fields] = await connection.query('SELECT * FROM dailyword')
+        dailyWord = result[0]['word']
+        res.send({ word: dailyWord })
+    } catch (err) {
+        console.error(err)
+    }
 })
 
-app.get('/load-highscores', (req, res) => {
-    connection.query('SELECT users.username AS Player, scores.correct AS Correct, scores.incorrect AS Incorrect FROM scores INNER JOIN users ON users.user_id=scores.user_id ORDER BY correct DESC LIMIT 10;', function (err, result, fields) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send({ data: result })
-        }
-    })
+app.get('/load-highscores', async (req, res) => {
+    try {
+        const [result, fields] = await connection.query('SELECT users.username AS Player, scores.correct AS Correct, scores.incorrect AS Incorrect FROM scores INNER JOIN users ON users.user_id=scores.user_id ORDER BY correct DESC LIMIT 10;')
+        res.send({ data: result })
+    } catch (err) {
+        console.error(err)
+    }
 })
 
-app.post('/new-user', function (req, res) {
+app.post('/new-user', async function (req, res) {
     let username = req.body.username;
     let password = req.body.password;
     let salt = bcrypt.genSaltSync(10);
@@ -73,18 +80,20 @@ app.post('/new-user', function (req, res) {
             username: username,
             password: passwordHash
         }
-        connection.query('INSERT INTO users SET ?', user, function (err, result) {
-            if (err) {
-                req.flash('error', err)
-                res.render('register')
-            } else {
-                req.flash('success', 'You have successfully signup!');
-                res.redirect('login');
+        try {
+            await connection.query('INSERT INTO users SET ?', user)
+            req.flash('success', 'You have successfully signup!');
+            res.redirect('login');
+        } catch (err) {
+            if (err.errno === 1062) {
+                req.flash('error', `Username "${username}" already exists, please choose another.`)
             }
-        })
+            res.render('register')
+        }
     } else {
         var error_msg = ''
         errors.forEach(function (error) {
+            console.error(err)
             error_msg += error.msg + '<br>'
         })
         req.flash('error', error_msg)
@@ -94,11 +103,11 @@ app.post('/new-user', function (req, res) {
 });
 
 
-app.post('/sign-in', function (req, res, next) {
+app.post('/sign-in', async function (req, res, next) {
     let username = req.body.username;
     let password = req.body.password;
-    connection.query('SELECT * FROM users WHERE username = ?', username, function (err, result, fields) {
-        if (err) throw err
+    try {
+        const [result, fields] = await connection.query('SELECT * FROM users WHERE username = ?', username)
         if (result.length <= 0) { // if user not found
             req.flash('error', 'Email or password incorrect')
             res.redirect('/login')
@@ -119,13 +128,12 @@ app.post('/sign-in', function (req, res, next) {
                     res.redirect('/login')
                 }
             } catch (err) {
-                console.log(err)
+                console.error(err)
             }
-
-
-
         }
-    })
+    } catch (err) {
+        console.error(err)
+    }
 }, updateHighscore)
 
 app.get('/logout', (req, res) => {
@@ -151,44 +159,43 @@ app.post('/highscore', function (req, res, next) {
     }
 }, updateHighscore)
 
-function updateHighscore(req, res, next) {
-    connection.query('SELECT user_id FROM users WHERE username = ?', req.session.name, function (err, result, fields) { // select user
-        let userID = result[0]['user_id']
-        connection.query("SELECT correct, incorrect, DATE_FORMAT(last_update_date, '%Y/%m/%d') AS last_update_date FROM scores WHERE user_id = ?", userID, function (err, result, fields) {
-            if (result.length <= 0) { // if user does not have a row in the score table create one
-                let correct
-                let incorrect
-                if (req.body.result) {
-                    correct = 1
-                    incorrect = 0
-                } else {
-                    correct = 0
-                    incorrect = 1
-                }
-                connection.query(`INSERT INTO scores (user_id, correct, incorrect, last_update_date) VALUES (${userID}, ${correct}, ${incorrect}, UTC_DATE())`, function (err, result, fields) {
-                    if (err) { return console.error(err.message) }
-                })
-            } else { // if user does have a row in scores table update it
-                let last_update_date = new Date(result[0]['last_update_date'].replace(/\//g, '-'))
-                let today = new Date()
-                if (last_update_date.getUTCDate() != today.getUTCDate() ||
-                    last_update_date.getUTCMonth() != today.getUTCMonth() ||
-                    last_update_date.getUTCFullYear() != today.getUTCFullYear()) { // only update row if user has not updated already today
-                    let userCorrectScore = result[0]['correct']
-                    let userIncorrectScore = result[0]['incorrect']
-                    if (req.body.result) {
-                        userCorrectScore += 1
-                    } else {
-                        userIncorrectScore += 1
-                    }
-                    connection.query(`UPDATE scores SET correct = ${userCorrectScore}, incorrect = ${userIncorrectScore}, last_update_date = UTC_DATE() WHERE user_id = ${userID}`, function (err, result, fields) {
-                        if (err) { return console.error(err.message) }
-                    })
-                }
 
+async function updateHighscore(req, res, next) {
+    try {
+        const userData = await connection.query('SELECT user_id FROM users WHERE username = ?', req.session.name) // select user
+        let userID = userData[0][0]['user_id']
+        const userScoreData = await connection.query("SELECT correct, incorrect, DATE_FORMAT(last_update_date, '%Y/%m/%d') AS last_update_date FROM scores WHERE user_id = ?", userID)
+        if (userScoreData[0].length <= 0) { // if user does not have a row in the score table create one
+            let correct
+            let incorrect
+            if (req.body.result) {
+                correct = 1
+                incorrect = 0
+            } else {
+                correct = 0
+                incorrect = 1
             }
-        })
-    })
+            await connection.query(`INSERT INTO scores (user_id, correct, incorrect, last_update_date) VALUES (${userID}, ${correct}, ${incorrect}, UTC_DATE())`)
+        } else { // if user does have a row in scores table update it
+            let last_update_date = new Date(result[0]['last_update_date'].replace(/\//g, '-'))
+            let today = new Date()
+            if (last_update_date.getUTCDate() != today.getUTCDate() ||
+                last_update_date.getUTCMonth() != today.getUTCMonth() ||
+                last_update_date.getUTCFullYear() != today.getUTCFullYear()) { // only update row if user has not updated already today
+                let userCorrectScore = result[0]['correct']
+                let userIncorrectScore = result[0]['incorrect']
+                if (req.body.result) {
+                    userCorrectScore += 1
+                } else {
+                    userIncorrectScore += 1
+                }
+                await connection.query(`UPDATE scores SET correct = ${userCorrectScore}, incorrect = ${userIncorrectScore}, last_update_date = UTC_DATE() WHERE user_id = ${userID}`)
+            }
+        }
+    }
+    catch (err) {
+        console.error(err)
+    }
     res.redirect('/')
 }
 
--- db.js
+++ db.js
@@ -1,26 +1,20 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config()
 
 
-const pool = mysql.createPool({
-    host: process.env.HOST_DB,
-    user: process.env.USER_DB,
-    password: process.env.PASSWORD_DB,
-    database: process.env.DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-
-pool.getConnection((err, connection) => {
-    if (err) {
-        console.log("A error has been occurred "
-            + "while connecting to database.");
-        throw err;
-    }
-    console.log('Database connected successfully');
-    pool.releaseConnection(connection);
-});
+try {
+    var pool = mysql.createPool({
+        host: process.env.HOST_DB,
+        user: process.env.USER_DB,
+        password: process.env.PASSWORD_DB,
+        database: process.env.DATABASE,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    })
+    console.log('databse connection pool created')
+} catch (err) {
+    console.error(err)
+}
 
 module.exports = pool; 
